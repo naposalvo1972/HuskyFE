864c08a1006bbf0aa4e652ad0431b6e67f7403d1
Salvatore Napolitano <snapolitano@sogei.it>
2022-03-17 16:26:13 +0100

    Altre modifiche nuove

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,7 +9,7 @@ function Navigation() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
               <NavLink className="navbar-brand" to="/">
-                  HUSK ME
+                  HUSKY ME
                 </NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
