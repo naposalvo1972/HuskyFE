f496e6d6d1aa0ac3b6cb2820512a851d570d6388
naposalvo1972 <95961303+naposalvo1972@users.noreply.github.com>
2022-05-22 17:07:53 +0200

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -13,7 +13,7 @@ function Alert() {
                     </p>
             </div>
             <div className="col-lg-12">
-               <iframe className="responsive-iframe"  height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=40696db1-a55d-47e8-ac3e-2911b2a72cb5&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+               <iframe title="aa" className="responsive-iframe"  height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=40696db1-a55d-47e8-ac3e-2911b2a72cb5&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
             
             </div>
                
