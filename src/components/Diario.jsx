905e7b2e4eeeb1bbb0ee923ccf2c960e5f331b84
naposalvo1972 <95961303+naposalvo1972@users.noreply.github.com>
2022-05-22 17:08:15 +0200

    Update Diario.jsx

--- src/components/Diario.jsx
+++ src/components/Diario.jsx
@@ -13,11 +13,11 @@ function Diario() {
                     </p>
             </div>
             <div className="col-lg-6">
-               <iframe className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=623319fe-eaf1-441e-8efe-4a95a87c2feb&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+               <iframe title="aa1" className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=623319fe-eaf1-441e-8efe-4a95a87c2feb&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
 
             </div>
             <div className="col-lg-6">
-               <iframe className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=6233221a-a4e5-4a4f-868c-8e686386424e&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+               <iframe title="aa2" className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=6233221a-a4e5-4a4f-868c-8e686386424e&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
 
             </div>      
       </div>
