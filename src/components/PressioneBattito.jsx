0772c52606f416552d978ccce68d4401fb2f5391
naposalvo1972 <95961303+naposalvo1972@users.noreply.github.com>
2022-05-22 17:09:20 +0200

    Update PressioneBattito.jsx

--- src/components/PressioneBattito.jsx
+++ src/components/PressioneBattito.jsx
@@ -12,11 +12,11 @@ function PressioneBattito() {
               </p>
             </div>
           <div className="col-lg-6">
-             <iframe className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=622f5c0d-f865-4a30-8967-66f75006cf60&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+             <iframe title="a1212a" className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=622f5c0d-f865-4a30-8967-66f75006cf60&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
             
           </div>
           <div className="col-lg-6">
-             <iframe className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=6231cbf5-aa9e-4bf6-8cf9-1abd29fff658&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+             <iframe title="a78a" className="responsive-iframe" height="500" Style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-sogeihack_2-23-22-eworksh-fejtr/embed/charts?id=6231cbf5-aa9e-4bf6-8cf9-1abd29fff658&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
           </div>
         </div>
       </div>
